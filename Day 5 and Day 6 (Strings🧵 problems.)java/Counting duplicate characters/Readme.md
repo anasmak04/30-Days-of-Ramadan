80fd1465a30c4cd1f58789046532ba070ca27da3
anasmak04 <anasdev8@gmail.com>
2023-03-27 16:23:10 +0000

    added counting duplicate character

---
+++ Day 5 and Day 6 (Strings🧵 problems.)java/Counting duplicate characters/Readme.md
@@ -0,0 +1,1 @@
+### counting duplicate characters
---
+++ Day 5 and Day 6 (Strings🧵 problems.)java/Counting duplicate characters/Solution.java
@@ -0,0 +1,29 @@
+*****************************************************************************
+
+// created at : 3/27/2023
+// problem : counting duplicate character
+
+
+*****************************************************************************
+
+    class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println(countDuplicateCharacters("heey cooders"));
+    
+    }
+
+    public Map<Character, Integer> countDuplicateCharacters(String str) {
+
+  Map<Character, Integer> result = new LinkedHashMap<>();
+
+    for(char c : ster.toCharArray()){
+        
+        result.compute(c, (k, v) -> (v == null) ? 1 : v++);
+
+    }
+      return result;
+  }
+
+
+
+    }
